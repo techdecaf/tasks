fc3146faf40f675e05973bb44d2fd62e41c469cf
Jon Ward <jon@techdecaf.com>
2020-08-07 09:09:01 -0700

    feature: improve init based on best pratices

--- cmd/init.go
+++ cmd/init.go
@@ -47,38 +47,41 @@ options:
   log: true
 
 variables:
-  CI_PROJECT_NAME: unset
-  CI_COMMIT_TAG: "{{EXEC ~~git describe --tags --always --dirty --abbrev=0~~}}"
-  CI_COMMIT_REF_NAME: "{{EXEC ~~git rev-parse --abbrev-ref HEAD~~}}"
-  CI_COMMIT_SHA: "{{EXEC ~~git rev-parse HEAD~~}}"
+  CI_PROJECT_NAME: "{{PWD | base}}"
+  CI_COMMIT_TAG: "{{TRY ~~git describe --tags --always --dirty --abbrev=0~~}}"
+  CI_COMMIT_REF_NAME: "{{TRY ~~git rev-parse --abbrev-ref HEAD~~}}"
+  CI_COMMIT_SHA: "{{TRY ~~git rev-parse HEAD~~}}"
 
 tasks:
   default:
     description: is the task that runs when no tasks have been specified.
     commands: [tasks list]
 
+  clean:
+    description: remove temporary files or directories
+    commands: []
+
   dependencies:
     description: install all required dependencies
+	commands: []
+
+  test:
+    description: run tests
+	commands: []
+	
+  coverage:
+    description: run test including coverage
     commands: []
 
   build:
     description: build current project
-    pre: [clean, dependencies]
-    commands: []
-
-  clean:
-    description: remove files created as part of the build step.
-    commands: []
-
-  test:
-    description: run tests
-    commands: []
-
-  lint:
-    description: run linting
     commands: []
 
   deploy:
     description: deploy the current project
+	commands: []
+	
+  upgrade:
+    description: upgrade the current project
     commands: []
 `
